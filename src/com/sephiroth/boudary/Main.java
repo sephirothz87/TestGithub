ac4122439bc7cfef0815d5253aab77fda70a7a19
sephirothz87 <sephiroth.zhong@gmail.com>
2015-04-15 11:40:24 +0800

    初始提交
    Signed-off-by: sephirothz87 <sephiroth.zhong@gmail.com>

---
+++ src/com/sephiroth/boudary/Main.java
@@ -0,0 +1,25 @@
+package com.sephiroth.boudary;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+	public static void main(String args[]){
+		System.out.println("hello,world.");
+		
+
+		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+		try {
+			System.out.print("输入第一个字符：");
+			String str1=br.readLine();
+			System.out.print("输入第二个字符：");
+			String str2=br.readLine();
+
+			System.out.println("第一个字符："+str1+" 第二个字符："+str2);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+}
